72e580c372f93a67500a212e774d2b1123526fc2
Nitika-git-design <nitikamohan123@gmail.com>
2025-07-11 15:33:21 +0530

    initial

---
+++ LongestWord.js
@@ -0,0 +1,1 @@
+//?Q.Write a function findLongestWord that takes a string as input and returns the longest word in the string,if there are multiple longest word then return the first one encountered.
